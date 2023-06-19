e75b9383606c2671af15f605b4add8dd53b832b8
kishan-2029 <kishan.prajapati@searce.com>
2023-06-19 17:16:05 +0530

    Navbar and Layout component created; Files add for the routing

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const NotFound = () => {
+    return (
+        <Layout>
+            <h1>You have entered the wrong url</h1>
+        </Layout>
+    )
+}
+
+export default NotFound
---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const about = () => {
+    return (<>
+        <Layout>
+            <h2>about</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A voluptas ipsum eveniet neque laudantium quam porro possimus eligendi asperiores? Veritatis illo reiciendis neque esse dignissimos nesciunt, architecto debitis accusamus voluptas.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A voluptas ipsum eveniet neque laudantium quam porro possimus eligendi asperiores? Veritatis illo reiciendis neque esse dignissimos nesciunt, architecto debitis accusamus voluptas.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A voluptas ipsum eveniet neque laudantium quam porro possimus eligendi asperiores? Veritatis illo reiciendis neque esse dignissimos nesciunt, architecto debitis accusamus voluptas.</p>
+        </Layout>
+    </>
+    )
+}
+
+export default about
