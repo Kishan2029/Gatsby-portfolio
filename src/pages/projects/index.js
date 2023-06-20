9e99fb80a4270e2338f28d10029fe49bb196d61a
kishan-2029 <kishan.prajapati@searce.com>
2023-06-20 18:23:39 +0530

    template for project-detail is generated

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Layout from '../../components/Layout'
 import * as styles from '../../styles/projects.module.css'
 import { Link, graphql } from 'gatsby'
+import { GatsbyImage } from "gatsby-plugin-image"
 
 const Projects = ({ data }) => {
     console.log("data", data);
@@ -16,6 +17,7 @@ const Projects = ({ data }) => {
                     {projects.map(project => (
                         <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
                             <div>
+                                <GatsbyImage image={project.frontmatter.thumb.childImageSharp.gatsbyImageData} alt="project image" />
                                 <h3>{project.frontmatter.title}</h3>
                                 <p>{project.frontmatter.stack}</p>
                             </div>
@@ -36,6 +38,11 @@ query {
           stack
           slug
           date
+          thumb {
+            childImageSharp {
+                gatsbyImageData(layout: FULL_WIDTH)
+            }
+          }
         }
         id
       }
