543eb725f23d3e45c09d0bed4e06e732de9f71c6
kishan-2029 <kishan.prajapati@searce.com>
2023-06-20 22:31:02 +0530

    Query vaiable is working in project route

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -1,21 +1,46 @@
 import React from "react"
+// import Img from "gatsby-image"
 import { GatsbyImage } from "gatsby-plugin-image"
 import Layout from "../components/Layout"
 import * as styles from '../styles/project-details.module.css'
+import { graphql } from 'gatsby'
 
-const ProjectDetails = () => {
+const ProjectDetails = ({ data }) => {
+    console.log("data", data)
+    const { html } = data.markdownRemark
+    const { title, stack, featuredImg } = data.markdownRemark.frontmatter
+
     return (
+
         <Layout>
             <div className={styles.details}>
-                <h2>title</h2>
-                <h3>stack</h3>
+                <h2>{title}</h2>
+                <h3>{stack}</h3>
                 <div className={styles.featured}>
-                    {/* <Img fluid={} /> */}
+                    <GatsbyImage image={featuredImg.childImageSharp.gatsbyImageData} alt="project image" />
+                    {/* <Img fluid={featuredImg.childImageSharp.fluid} /> */}
                 </div>
-                {/* <div className={styles.html} dangerouslySetInnerHTML={} /> */}
+                <div className={styles.html} dangerouslySetInnerHTML={{ __html: html }} />
             </div>
         </Layout>
     )
 }
 
 export default ProjectDetails
+
+export const query = graphql`
+query MyQuery($slug: String) {
+    markdownRemark(frontmatter: {slug: {eq: $slug}}){
+      html
+      frontmatter{
+        stack
+        title
+        featuredImg {
+              childImageSharp {
+                  gatsbyImageData(layout: FULL_WIDTH)
+              }
+          }
+      }
+    }
+  }
+`
